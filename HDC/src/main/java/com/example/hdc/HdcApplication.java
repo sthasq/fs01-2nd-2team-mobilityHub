06af0c810bbca9aa440a9a262a931351dbb3f230
heewonn09 <heewonn09@naver.com>
2025-12-30 23:52:20 +0900

    수정
    관리자 입출구 페이지에 등록된 차량 번호 입출구 페이지에 조회기능  카메라 확인 후 실시간으로 관리자가 입차 승인을 눌러 입구를 직접 작동 하게 수정 함

---
+++ HDC/src/main/java/com/example/hdc/HdcApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdcApplication.class, args);
+	}
+
+}
---
+++ HDC/src/test/java/com/example/hdc/HdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
